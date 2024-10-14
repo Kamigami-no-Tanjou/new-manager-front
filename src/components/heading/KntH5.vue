33f1625f8d490d0bbd6c750cc42948f772d0130f
RedNeath <theandre.bourry@ik.me>
2024-10-14 02:36:45 +0200

    [FEAT] Added custom h5 component

---
+++ src/components/heading/KntH5.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: false,
+    default: ''
+  }
+});
+</script>
+
+<template>
+  <h5
+    class="mb-3 text-xl font-medium text-gray-900 antialiased lg:mb-1 dark:text-white"
+  >
+    {{ text }}
+  </h5>
+</template>
--- src/views/ComponentLibraryView.vue
+++ src/views/ComponentLibraryView.vue
@@ -2,11 +2,13 @@
 import KntH1 from '../components/heading/KntH1.vue'
 import KntBreadcrumb from '@/components/heading/KntBreadcrumb.vue'
 import Breadcrumb from '@/classes/Breadcrumb.js'
+import KntH5 from '@/components/heading/KntH5.vue';
 </script>
 
 <template>
   <main class="library h-screen bg-white dark:bg-gray-900">
     <KntH1 text="KntH1" />
+    <KntH5 text="KntH5" />
     <KntBreadcrumb :breadcrumbs="[new Breadcrumb('Dashboard', '#'), new Breadcrumb('Page 1', '#'), new Breadcrumb('Page 2', null)]" :mobile-breadcrumb="new Breadcrumb('Back to page 1', '#')" />
   </main>
 </template>
