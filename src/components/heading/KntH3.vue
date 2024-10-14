c8e52f9fe73936b0b6e10427f971659dce908098
RedNeath <theandre.bourry@ik.me>
2024-10-15 00:50:03 +0200

    [FEAT] Added h3 custom component

---
+++ src/components/heading/KntH3.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: false,
+    default: ''
+  }
+})
+</script>
+
+<template v-slot="title">
+  <h3
+    class="mb-5 text-3xl text-gray-900 font-bold antialiased lg:mb-3 lg:text-3xl dark:text-white"
+  >
+    {{ text }}
+  </h3>
+</template>
--- src/views/ComponentLibraryView.vue
+++ src/views/ComponentLibraryView.vue
@@ -3,8 +3,8 @@
 import Breadcrumb from '@/classes/Breadcrumb.js';
 
 import KntH1 from '../components/heading/KntH1.vue';
-import KntBreadcrumb from '@/components/heading/KntBreadcrumb.vue';
 import KntH2 from '@/components/heading/KntH2.vue';
+import KntH3 from '@/components/heading/KntH3.vue';
 import KntH5 from '@/components/heading/KntH5.vue';
 import KntSmallText from '@/components/heading/KntSmallText.vue';
 import KntHomeTile from '@/components/heading/KntHomeTile.vue';
@@ -20,6 +20,7 @@ import { HomeTileIconsEnum } from '@/enums/HomeTileIconsEnum.js';
   <main class="library min-h-screen bg-white dark:bg-gray-900">
     <KntH1 text="KntH1" />
     <KntH2 text="KntH2" />
+    <KntH3 text="KntH3" />
     <KntH5 text="KntH5" />
     <KntBreadcrumb :breadcrumbs="[new Breadcrumb('Dashboard', '#'), new Breadcrumb('Page 1', '#'), new Breadcrumb('Page 2', null)]" :mobile-breadcrumb="new Breadcrumb('Back to page 1', '#')" />
     <KntSmallText text="This is a small text! :D" />
