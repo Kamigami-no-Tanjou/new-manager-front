8f87dad7b8f63d2e10472a15f562f64137678ab7
RedNeath <theandre.bourry@ik.me>
2024-10-23 23:00:33 +0200

    [FEAT] Added the paragraph component

---
+++ src/components/text/KntParagraph.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <p class="text-base text-gray-900 dark:text-white">{{ text }}</p>
+</template>
